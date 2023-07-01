2a7642ae18095be3cb053fe95d26bd383fef274b
Igor Tarasenko <igtarako@gmail.com>
2023-07-01 21:28:18 +0700

    * тест регистрации

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -17,6 +17,6 @@ func StringContains(t *testing.T, actual, expectedSubstring string) {
 	t.Helper()
 
 	if !strings.Contains(actual, expectedSubstring) {
-		t.Errorf("gor: %q, expected to cotain: %q", actual, expectedSubstring)
+		t.Errorf("got: %q, expected to cotain: %q", actual, expectedSubstring)
 	}
 }
